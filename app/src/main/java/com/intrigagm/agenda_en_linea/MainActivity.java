871e435897f6bd1ca07d56a447d1e8250cbdb901
FernandoGM2701 <fernandogm2701@gmail.com>
2022-08-26 21:26:25 -0500

    Initial project

---
+++ app/src/main/java/com/intrigagm/agenda_en_linea/MainActivity.java
@@ -0,0 +1,14 @@
+package com.intrigagm.agenda_en_linea;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
